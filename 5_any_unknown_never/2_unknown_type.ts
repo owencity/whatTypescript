6f54165ffe34cfc681fcdf38ee307837f22a9faa
owencity <skyko6530@naver.com>
2024-11-04 23:12:31 +0900

    unknown 타입 주의

---
+++ 5_any_unknown_never/2_unknown_type.ts
@@ -0,0 +1,43 @@
+/* 
+    Unknown Type
+*/
+
+let anyValue: any;
+
+anyValue = 24;
+anyValue = '아이유';
+anyValue = false;
+anyValue = [];
+anyValue = {};
+anyValue = null;
+anyValue = undefined;
+
+let unknownValue: unknown;
+
+unknownValue = 24;
+unknownValue = '아이유';
+unknownValue = false;
+unknownValue = [];
+unknownValue = {};
+unknownValue = null;
+unknownValue = undefined;
+
+// 할당에서의 차이 
+
+// let anyType: any = anyValue;
+// let unknownType: unknown = anyValue;
+// let booleanType: boolean = anyValue;
+// let arrayType: string[] = anyValue;
+// let objectType: {} = anyValue;
+// let nullType: null = anyValue;
+// let undefinedType: undefined = anyValue;
+
+
+let anyType: any = ;
+let unknownType: unknown = ;
+let booleanType: boolean = ;
+let arrayType: string[] = ;
+let objectType: {} = ;
+let nullType: null = ;
+let undefinedType: undefined = ;
+
